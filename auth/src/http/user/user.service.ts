b504d17b162e2841e4da0d92e5642119aa21d109
Daniel Soares <daniel.soares@me.com>
2022-04-06 01:06:24 -0300

    feat(auth): add prisma service

--- auth/src/http/user/user.service.ts
+++ auth/src/http/user/user.service.ts
@@ -1,27 +1,41 @@
-import { Injectable } from '@nestjs/common'
+import { BadRequestException, Injectable } from '@nestjs/common'
 
+import { PrismaService } from '../../prisma/prisma.service'
 import { CreateUserInput } from './input/create-user.input'
 import { UpdateUserInput } from './input/update-user.input'
 
 @Injectable()
 export class UserService {
-  createUser(data: CreateUserInput) {
-    return 'This action adds a new user'
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async createUser(data: CreateUserInput) {
+    const emailExists = await this.prismaService.user.findUnique({
+      where: { email: data.email }
+    })
+
+    if (emailExists) {
+      throw new BadRequestException('Email already exists')
+    }
+
+    return await this.prismaService.user.create({ data })
   }
 
-  findAll() {
-    return `This action returns all user`
+  async findAll() {
+    return await this.prismaService.user.findMany()
   }
 
-  findOne(id: string) {
-    return `This action returns a #${id} user`
+  async findOne(id: string) {
+    return await this.prismaService.user.findUnique({ where: { id } })
   }
 
-  updateUser(id: string, data: UpdateUserInput) {
-    return `This action updates a #${id} user`
+  async updateUser(id: string, data: UpdateUserInput) {
+    return await this.prismaService.user.update({
+      where: { id },
+      data
+    })
   }
 
-  removeUser(id: string) {
-    return `This action removes a #${id} user`
+  async removeUser(id: string) {
+    return await this.prismaService.user.delete({ where: { id } })
   }
 }
