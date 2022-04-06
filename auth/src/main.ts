c2c9c2866a54f889cdc08730faf5fc0d602ddcc4
Daniel Soares <daniel.soares@me.com>
2022-04-05 23:12:18 -0300

    first commit

---
+++ auth/src/main.ts
@@ -0,0 +1,20 @@
+import { ConfigService } from '@nestjs/config'
+import { NestFactory } from '@nestjs/core'
+import { NestExpressApplication } from '@nestjs/platform-express'
+
+import { AppModule } from './app.module'
+
+async function bootstrap() {
+  const app: NestExpressApplication = await NestFactory.create(AppModule, {
+    cors: true,
+    logger: ['error', 'warn']
+  })
+
+  const config: ConfigService = app.get(ConfigService)
+  const port = config.get<number>('PORT', 3010)
+
+  console.log(`Auth service is listening on port ${port}`)
+  await app.listen(port)
+}
+
+bootstrap()
